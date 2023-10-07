484621c1bf2f7ec97b195fd34e51603fbd8f073b
Murad <murad.yunus.2017@gmail.com>
2023-10-07 21:42:28 +0400

    Add project files.

---
+++ DataStructures/MyQueue.cs
@@ -0,0 +1,44 @@
+﻿namespace DataStructures
+{
+    public class MyQueue<T>
+    {
+        private const int DEFAULT_SIZE = 50;
+
+        private T[] elements;
+        private int head = 0;
+        private int tail = -1;
+        private int count = 0;
+        public MyQueue(int initial = DEFAULT_SIZE)
+        {
+            elements = new T[initial];
+        }
+
+        public void Enqueue(T item)
+        {
+            tail++;
+            elements[tail] = item;
+            count++;
+        }
+
+        public T Dequeue()
+        {
+            ThrowIfEmpty();
+
+            T item = elements[head];
+            elements[head] = default;
+            head++;
+            count--;
+
+            return item;
+        }
+
+        private void ThrowIfEmpty()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException("Queue is empty");
+        }
+
+        public bool IsEmpty() => count == 0;
+
+    }
+}
---
+++ DataStructures/Program.cs
@@ -0,0 +1,10 @@
+﻿using DataStructures;
+
+MyQueue<int> test = new MyQueue<int>(5);
+
+test.Enqueue(2);
+test.Enqueue(3);
+test.Enqueue(4);
+test.Enqueue(5);
+
+Console.WriteLine(test.Dequeue());
