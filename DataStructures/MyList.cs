89fb018daff8274e973f59f273e814e42519ee58
Murad <murad.yunus.2017@gmail.com>
2023-12-10 11:48:45 +0400

    List add and resize method

---
+++ DataStructures/MyList.cs
@@ -0,0 +1,35 @@
+﻿namespace DataStructures
+{
+    public class MyList<T>
+    {
+        private T[] items;
+        private int count;
+
+        public MyList()
+        {
+            items = new T[1];
+            count = 0;
+        }
+
+        public void Add(T item)
+        {
+            if (items.Length == count)
+            {
+                Resize(count * 2);
+            }
+            items[count++] = item;
+        }
+
+        private void Resize(int newSize)
+        {
+            T[] newItems = new T[newSize];
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                newItems[i] = items[i];
+            }
+
+            items = newItems;
+        }
+    }
+}
