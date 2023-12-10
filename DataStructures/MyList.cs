2b6409436bb67c8d832acd9ab483776ef71ed34e
Murad <murad.yunus.2017@gmail.com>
2023-12-10 11:54:50 +0400

    IndexOf method

--- DataStructures/MyList.cs
+++ DataStructures/MyList.cs
@@ -10,7 +10,24 @@
             items = new T[1];
             count = 0;
         }
+        #region Properties
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+        public int Capacity
+        {
+            get
+            {
+                return items.Length;
+            }
+        }
+        #endregion
 
+        #region Methods
         public void Add(T item)
         {
             if (items.Length == count)
@@ -18,6 +35,34 @@
                 Resize(count * 2);
             }
             items[count++] = item;
+        }
+
+        public void Remove(T item)
+        {
+            int index = IndexOf(item);
+
+            if (index == -1)
+                return;
+
+            for (int i = index; i < count - 1; i++)
+            {
+                items[i] = items[i + 1];
+            }
+
+            if (items.Length / 4 == count)
+            {
+                Resize(items.Length / 2);
+            }
+        }
+
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (items[i]!.Equals(item))
+                    return i;
+            }
+            return -1;
         }
 
         private void Resize(int newSize)
@@ -31,5 +76,6 @@
 
             items = newItems;
         }
+        #endregion
     }
 }
